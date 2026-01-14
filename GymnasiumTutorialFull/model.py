95ec2e26d535835bc2f7a8456db6580fe2219e27
Brett Hobbs <bretthhobbs@gmail.com>
2026-01-14 17:28:30 -0500

    [TEST] try A2C instead of DQN

--- GymnasiumTutorialFull/model.py
+++ GymnasiumTutorialFull/model.py
@@ -1,4 +1,4 @@
-from stable_baselines3 import DQN
+from stable_baselines3 import DQN, A2C
 import gymnasium as gym
 import time
 import simple_coverage
@@ -6,13 +6,13 @@ from stable_baselines3.common.env_util import make_vec_env
 
 # Parallel environments for training
 vec_env = make_vec_env("complete", n_envs=4)
-model = DQN("MlpPolicy", vec_env, verbose=1, tensorboard_log="./tensorboard/")
+model = A2C("MlpPolicy", vec_env, verbose=1, tensorboard_log="./tensorboard/")
 model.learn(total_timesteps=10000, tb_log_name="dqn_run_1_1")
 model.save("gridworld1_1")
 
 # Single environment for testing
 env = gym.make("complete", render_mode="human")
-model = DQN.load("gridworld1_1")
+model = A2C.load("gridworld1_1")
 obs, _ = env.reset()
 terminated = False
 total_reward = 0
